540104c2dba5bc440d889615312804507a1aca51
Minh2k3 <minhhblh31@gmail.com>
2025-08-19 22:55:14 +0700

    fix get storage

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'api/*/*', 'api/documents/*', 'sanctum/csrf-cookie', 'login', 'logout', 'password/reset', 'password/email', 'password/confirm', 'user/verify-email/*', 'user/*', 'documents/*', 'reset-password', 'verify-reset-token', 'forgot-password', 'direct-pusher', '*', 'wishes' ],
+    'paths' => ['api/*', 'storage/*', 'api/*/*', 'api/documents/*', 'sanctum/csrf-cookie', 'login', 'logout', 'password/reset', 'password/email', 'password/confirm', 'user/verify-email/*', 'user/*', 'documents/*', 'reset-password', 'verify-reset-token', 'forgot-password', 'direct-pusher', '*', 'wishes' ],
 
     'allowed_methods' => ['PUT', 'POST', 'GET', 'DELETE'],   
 
